937c1e9a0a95ea5c8d1a3126c2e2bb329dc652b7
syuusyuusy <shuji20100813@gmail.com>
2026-05-22 22:47:02 +0900

    Update route.ts

--- app/api/reserve/route.ts
+++ app/api/reserve/route.ts
@@ -2,7 +2,7 @@ export async function POST(req: Request) {
   const body = await req.json();
 
   const GAS_URL =
-    "https://script.google.com/macros/s/AKfycbw4ljJo3oCxvzK62WXboP-Tf0031XQ5mSGNv9WIo8n2InTfrcJt_oZh8wAfIkxvyP5s/exec";
+    "https://script.google.com/macros/s/AKfycbwpdEk6rrGHsXW5WwyMRk2ggpULqNT3-WcJYTYFV5oK5QsDiAnAqGH2cGcoUWfAvjK8hQ/exec";
 
   try {
     const res = await fetch(GAS_URL, {
