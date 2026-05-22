7e7784198ebfc1c4e035aa18957d7db62a68e426
syuusyuusy <shuji20100813@gmail.com>
2026-05-22 22:16:19 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
 
   // 自分のGAS URL
   const GAS_URL =
-    "https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXX/exec";
+    "https://script.google.com/macros/s/AKfycbw4ljJo3oCxvzK62WXboP-Tf0031XQ5mSGNv9WIo8n2InTfrcJt_oZh8wAfIkxvyP5s/exec";
 
   // 空き状況取得
   const loadSlots = async () => {
