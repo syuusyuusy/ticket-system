079e8ec0700b5e2f86793379f3562e9f141b038b
@Tomatochip001 <151353333+Tomatochip001@users.noreply.github.com>
2026-06-29 13:46:40 +0900

    Update GAS_URL to new endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function Page() {
 
   // GAS URL
   const GAS_URL =
-    "https://script.google.com/macros/s/AKfycbz1UXKEWn0-gB8ILbOnexruIaKU81Qf_sGeHQ0u8iTgkUzv37oBmenaazRvBYA46a5PAw/exec";
+    "https://script.google.com/macros/s/AKfycbwULF0fpoWAjGlubEem8beeP8ViozQ2YWGhNNj5b61ACAhA7OhwmNvxGWJ4Yuj3eRUQ7Q/exec";
 
   // 空き状況取得
   const loadSlots = async () => {
